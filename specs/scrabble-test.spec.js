6df036548301e80670fd05a5db43211745eaf40a
Colin <cln.gardiner@outlook.com>
2021-06-29 17:23:40 +0100

    created unit tests

---
+++ specs/scrabble-test.spec.js
@@ -0,0 +1,20 @@
+// Load required modules to run the unit tests.
+const Scrabble = require("../src/scrabble.js");
+const assertEqual = require("../src/testing-framework");
+
+//Initialise variables
+const inputWord, expectedScore, actualScore
+
+// Setup 
+console.log('Assigning an input to a word');
+inputWord = 'word';
+console.log('Creating a new instance of a Scrabble Class object');
+const scrabbleWord = new Scrabble(inputWord);
+
+// Exercise 
+expectedScore = scrabbleWord.score();
+
+// Verify
+expectedScore = '' ;actualScore = scrabbleWord.score();
+
+console.log(assertEqual(expectedScore,actualScore));
