25d5f55acaa91eb6ae536649c9aa3ea1fdb75892
Colin <cln.gardiner@outlook.com>
2021-06-29 17:44:49 +0100

    added working specRunner file

---
+++ specRunner.js
@@ -0,0 +1,20 @@
+// Loop over all spec.js files in the spec directory
+const path = require('path');
+const fs = require('fs');
+
+const filePath = "/Users/cmg18/Digital Futures/Challenges/scrabble-challenge-tdd/specRunner.js";
+
+const dirPath = path.join(__dirname,'src');
+
+fs.readdir(dirPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(`Read ${file}`);
+        require(`${dirPath}/${file}`);
+    });
+});
